9901b9976c151f8164bf96f980d5526f2f6b6e9c
mdmarufsarker <mdmarufsarker2021@gmail.com>
2022-07-14 00:09:22 +0600

    readme, db file updated

---
+++ README.md
@@ -0,0 +1,7 @@
+# Note Taking App
+
+## Tools Used
+
+`nodejs` `expressjs` `mongodb` `mongoose` `reactjs` `react-icons` `html` `css` `nodemon` `chalk`
+
+![Project Screenshots](./project.png)
--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,12 +1,38 @@
 import React from "react";
+import { FiEdit } from "react-icons/fi";
+import { GrUpdate } from "react-icons/gr";
 
-const Card = ({note}) => {
+const Card = ({ note }) => {
   return (
     <div className="card">
-      <h3>{note.title}</h3>
+      <div
+        className="editUpdate"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <span style={{ fontWeight: "medium", fontSize: "30px" }}>
+          {note.title}
+        </span>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            gap: "10px",
+          }}
+        >
+          <FiEdit />
+          <GrUpdate />
+        </div>
+      </div>
+      <hr style={{marginBottom: "5px"}}/>
       <p>{note.description}</p>
       <p>
-        <strong>Last Date: </strong>{note.lastDate}
+        <strong>Last Date: </strong>
+        {note.lastDate}
       </p>
     </div>
   );
