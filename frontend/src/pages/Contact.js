89243f20f6c903c8de978eaa467dd7607a5d6bdd
mdmarufsarker <mdmarufsarker2021@gmail.com>
2022-07-15 17:22:15 +0600

    emailjs integrated

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,7 +1,49 @@
-import React from "react";
+import { useEffect, useState } from "react";
 // import { Link } from "react-router-dom";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [status, setStatus] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // emailjs.send(serviceID, templateID, templateParams, publicKey);
+    emailjs
+      .send("service_hlpp5v7", "template_vw5thuq", values, "xGlQWSinaq5g6MroQ")
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response);
+          setValues({
+            name: "",
+            email: "",
+            message: "",
+          });
+          setStatus("SUCCESS");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+  };
+  useEffect(() => {
+    if (status === "SUCCESS") {
+      setTimeout(() => {
+        setStatus("");
+      }, 3000);
+    }
+  }, [status]);
+  const handleChange = (e) => {
+    setValues((values) => ({
+      ...values,
+      [e.target.name]: e.target.value,
+    }));
+  };
   return (
     <div className="contact-container">
       <div className="container">
@@ -33,18 +75,48 @@ const Contact = () => {
               tutorial, you can send me message from here. It's my pleasure to
               help you.
             </p>
-            <form action="mailto:mdmarufsarker.mms@gmail.com" method="post" enctype="text/plain">
+            <form onSubmit={handleSubmit}>
               <div className="input-box">
-                <input type="text" placeholder="Enter your name" />
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  onChange={handleChange}
+                  value={values.name}
+                  name="name"
+                  label="Full Name"
+                  required
+                />
               </div>
               <div className="input-box">
-                <input type="text" placeholder="Enter your email" />
+                <input
+                  type="text"
+                  placeholder="Enter your email"
+                  onChange={handleChange}
+                  value={values.email}
+                  name="email"
+                  label="Email"
+                  required
+                />
               </div>
               <div className="input-box message-box">
-                <textarea placeholder="Enter your message" col="30" row="5"/>
+                <textarea
+                  placeholder="Enter your message"
+                  col="30"
+                  row="5"
+                  onChange={handleChange}
+                  value={values.message}
+                  name="message"
+                  label="Message"
+                  required
+                />
               </div>
-              <div className="button">
-                <input type="submit"  value="Send Now" />
+              <div className="button" style={{display: "flex"}}>
+                <input
+                  type="submit"
+                  value="Send Now"
+                  style={{ marginRight: "10px" }}
+                />
+                {status && renderAlert()}
               </div>
             </form>
           </div>
@@ -53,5 +125,10 @@ const Contact = () => {
     </div>
   );
 };
+const renderAlert = () => (
+  <div className="redAlert">
+    <p>your message submitted successfully</p>
+  </div>
+);
 
 export default Contact;
